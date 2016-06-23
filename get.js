a2558cc745f6e43b7487645b5102a69c7d6a80f9
Aleksey Smolenchuk <aleksey@uber.com>
2016-06-22 21:06:13 -0700

    added semicolons

--- get.js
+++ get.js
@@ -1,8 +1,8 @@
-var window = require("global/window")
+var window = require("global/window");
 
-module.exports = getJSONGlobal
+module.exports = getJSONGlobal;
 
 function getJSONGlobal(key) {
-    var globals = window.__JSON_GLOBALS_ || {}
-    return globals[key]
+    var globals = window.__JSON_GLOBALS_ || {};
+    return globals[key];
 }
