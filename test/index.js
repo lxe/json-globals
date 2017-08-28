2ed6a0130c13565922987f3ebb07c8eaae66f6e2
Mo Kouli <koulmomo@users.noreply.github.com>
2017-08-28 10:58:25 -0700

    [refactor] "replace" serialize-javascript with JSON.stringify (#6)
    will require a major version bump after landing.
    
    :art: We pass the [`isJSON` option](https://github.com/yahoo/serialize-javascript/tree/2b1e4c78e3be3246390e2f723da70042cc4fcaf3#optionsisjson) to `seralize-javascript`
    
    adding `isJSON` option makes it so that serialize-javascript behaves **EXACTLY** like `JSON.stringify` **BUT** sanitizes against XSS
    dates are serialized as strings, regex are empty objects, and functions are undefined (ie do not get stringified)
    
    :white_check_mark: update existing unit test and add unit test for `get.js`
    
    :memo: add code comments

--- test/index.js
+++ test/index.js
@@ -11,14 +11,21 @@ test("Can globalify stuff", function (assert) {
     var html = JSONGlobals({
         plain: { name: "bob" },
         regex: /foo/,
-        malicious: '</script>'
+        malicious: '</script>',
+        date: new Date("2017-08-25T21:26:25.690Z"),
+        func: function foo() {
+            return 'bar';
+        }
     })
 
     assert.equal(html,
       '<script id="json-globals" type="application/json">' +
-      '{"plain":{"name":"bob"},"regex":/foo/,"malicious":"\\u003C\\u002Fscript\\u003E"}' +
+      '{"plain":{"name":"bob"},"regex":{},"malicious":"\\u003C\\u002Fscript\\u003E","date":"2017-08-25T21:26:25.690Z"}' +
       '</script>'
     );
 
     assert.end()
 })
+
+// run other tests
+require('./test-get')
